77acef457e84c9f29021bcd6f8ff2e3364e8965b
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-10-31 14:50:25 +1100

    chore: update install_topsbm script for clarity

--- scripts/install_topsbm.sh
+++ scripts/install_topsbm.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 # This script installs topsbm into your local conda virtual environment.
+# make sure you have activated your conda environment.
 
 HELP="./$(basename $0) <user-defined topsbm package name>"
 
@@ -7,6 +8,7 @@ if [[ -z $1 ]]; then
     echo $HELP;
     exit 1
 fi
+PKG_NAME=$1
 
 SITEPACKAGES_DIR=$(python <<EOF
 import site
@@ -17,7 +19,7 @@ else:
   raise RuntimeError("Expecting one site-packages path only")
 EOF
 )
-PKG_PATH="${SITEPACKAGES_DIR}/$1"
+PKG_PATH="${SITEPACKAGES_DIR}/${PKG_NAME}"
 echo "PYTHON=$(which python)"
 echo "SITEPACKAGES=$SITEPACKAGES_DIR"
 echo "INSTALL_TO=$PKG_PATH"
@@ -27,3 +29,4 @@ printf "Confirm? (y/n): " && read x && [[ $x == 'y' ]] || exit 1
 echo "Cloning to $PKG_PATH..."
 git clone --quiet https://github.com/martingerlach/hSBM_Topicmodel.git $PKG_PATH
 echo "Done. Cloned to $PKG_PATH"
+echo "You may now import ${PKG_NAME}."
