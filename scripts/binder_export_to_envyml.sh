fa709f3d792655fae52209c2a5f182c096753ed1
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-11-03 11:22:00 +1100

    chore: fix binder export script

--- scripts/binder_export_to_envyml.sh
+++ scripts/binder_export_to_envyml.sh
@@ -8,12 +8,14 @@ echo "PYTHON=$(which python) version=$(python --version)"
 echo "CONDA=$CONDA_EXE version=$(conda --version)"
 echo "EXPORT_TO=$EXPORT_TO"
 
-echo "Please check above for correct environment to export from."
-printf "Confirm? (y/n): " && read -x && [[ $x == 'y' ]] || exit 0
+echo "++ Please check above for correct environment to export from."
+printf "Confirm? (y/n): " && read x && [[ $x == 'y' ]] || exit 0
 
 if [[ -f $EXPORT_TO ]]; then
-  printf "$EXPORT_TO exists. Replace? (y/n): " && read -x && [[ $x == 'y' ]] || exit 0
+  printf "-- $EXPORT_TO exists. Replace? (y/n): " && read x && [[ $x == 'y' ]] || exit 0
 fi
 
 # unwanted 'PREFIX' when using conda env export - removed with tail chain
 conda env export | tail -r | tail -n +2 | tail -r | grep -v 'jupyter' > $EXPORT_TO
+
+echo "++ Done. Exported to $EXPORT_TO"
