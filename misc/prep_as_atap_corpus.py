a9e903c35e968f563ef9e5e7e6b100b308a01cab
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-05-02 10:16:26 +1000

    feat: prep_as_atap_corpus - rm Category: (wiki) and Add category (arxiv)

--- misc/prep_as_atap_corpus.py
+++ misc/prep_as_atap_corpus.py
@@ -8,27 +8,33 @@ import os
 from enum import Enum
 from pathlib import Path
 from typing import Self, Iterator
+import re
 
 import pandas as pd
 
+DATA_DIR: Path = Path(f"{os.path.dirname(__file__)}/dataTopSBM")
+assert DATA_DIR.is_dir(), f"{DATA_DIR.absolute()} directory does not exist."
+
 
 class WikiPath(Enum):
-    document: str = f"{os.path.dirname(__file__)}/wikipedia-texts.txt"
-    title: str = f"{os.path.dirname(__file__)}/wikipedia-titles.txt"
-    meta_nodes: str = f"{os.path.dirname(__file__)}/wikipedia-metadataNodes.txt"
-    meta_edges: str = f"{os.path.dirname(__file__)}/wikipedia-metadataEdgelist.txt"  # this will be unused for workshop 08.May.24
+    document: str = DATA_DIR.joinpath("wikipedia-texts.txt")
+    title: str = DATA_DIR.joinpath("wikipedia-titles.txt")
+    meta_nodes: str = DATA_DIR.joinpath("wikipedia-metadataNodes.txt")
+    meta_edges: str = DATA_DIR.joinpath(
+        "wikipedia-metadataEdgelist.txt"
+    )  # this will be unused for workshop 08.May.24
 
 
 class ArxivPath(Enum):
-    document: str = f"{os.path.dirname(__file__)}/arxiv-texts.txt"
-    title: str = f"{os.path.dirname(__file__)}/arxiv-titles.txt"
-    meta_nodes: str = f"{os.path.dirname(__file__)}/arxiv-metadataNodes.txt"
-    meta_edges: str = f"{os.path.dirname(__file__)}/arxiv-metadataEdgelist.txt"  # this will be unused for workshop 08.May.24
+    document: str = DATA_DIR.joinpath("arxiv-texts.txt")
+    title: str = DATA_DIR.joinpath("arxiv-titles.txt")
+    meta_nodes: str = DATA_DIR.joinpath("arxiv-metadataNodes.txt")
+    meta_edges: str = DATA_DIR.joinpath("arxiv-metadataEdgelist.txt")
 
 
 class ConstitutionsPath(Enum):
-    document: str = f"{os.path.dirname(__file__)}/constitutions-texts.txt"
-    title: str = f"{os.path.dirname(__file__)}/constitutions-titles.txt"
+    document: str = DATA_DIR.joinpath("constitutions-texts.txt")
+    title: str = DATA_DIR.joinpath("constitutions-titles.txt")
 
 
 class Dataset(str, Enum):
@@ -73,6 +79,17 @@ def parse_wiki(out: str) -> str:
     )
     df["title"] = pd.read_table(WikiPath.title.value, header=None)
     df = pd.merge(df, pd.read_csv(WikiPath.meta_nodes.value, sep="\t"), on="title")
+    assert (
+        "category" in df.columns
+    ), f"Missing 'category' column from {WikiPath.meta_nodes.value}."
+    assert (
+        "subcategory" in df.columns
+    ), f"Missing 'subcategory' column from {WikiPath.meta_nodes.value}."
+
+    df["category"] = df["category"].apply(lambda c: c.replace("Category:", "").strip())
+    df["subcategory"] = df["subcategory"].apply(
+        lambda c: c.replace("Category:", "").strip()
+    )
     df.to_csv(out, index=False)
     return out
 
@@ -86,7 +103,8 @@ def parse_arxiv(out: str) -> str:
         index_col=False,
     )
     df["title"] = pd.read_table(ArxivPath.title.value, header=None)
-    # note: nodes and edgelists only describes their relationship with no extra metadata hence skipped.
+    df["category"] = pd.read_table(ArxivPath.meta_nodes.value, header=None).loc[:, 1]
+    df["category"] = df["category"].apply(lambda c: c.replace("'", "").strip())
     df.to_csv(out, index=False)
     return out
 
